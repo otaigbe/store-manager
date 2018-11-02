ba3ee909e3af838a43cc330d438fda3bccfec491
otaigbe <stanlex4400@gmail.com>
2018-11-02 04:53:31 +0100

    pushing to git

--- src/controllers/db.js
+++ src/controllers/db.js
@@ -1,8 +1,11 @@
 import pg from 'pg';
 
 const config = {
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  //user: process.env.PGUSER,
+  database: process.env.DATABASE_URL
+  //password: process.env.PGPASSWORD,
+  //port: process.env.PGPORT,
 };
+
 const pool = new pg.Pool(config);
 export default pool;
