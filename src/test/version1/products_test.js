741140243a1f1d44e24b330f6eca66b2a1abfc52
otaigbe <stanlex4400@gmail.com>
2018-11-12 19:45:07 +0100

    Adding coverage

--- src/test/version1/products_test.js
+++ src/test/version1/products_test.js
@@ -71,25 +71,25 @@ describe('Testing out Products endpoints', () => {
           done();
         });
     });
-    // it('POST / products endpoint; should update an already existing product in the database', co.wrap(() => {
-    //   chai.request(app)
-    //     .post('/api/v1/products')
-    //     .set('x-auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im90YWlnYmVAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU0MTQ4NjQ2MH0.F-7ZK_IyOxO5VVKlotO7ySh5QF4Bz2T3qNEg0CxDNSI')
-    //     .type('form')
-    //     .send({
-    //       product_desc: 'bucket',
-    //       unit_price: 350,
-    //       quantity_supplied: 10,
-    //       supplier_name: 'Okonkwo',
-    //       category: 'hardware',
-    //     })
-    //     .end((err, res) => {
-    //       expect(err).to.be.null;
-    //       expect(res.body).to.be.a.jsonObj();
-    //       expect(res).to.have.status(200);
-    //       expect(res.body).to.eql({ message: 'Updated an already existent product.' });
-    //     });
-    // }));
+    it('POST / products endpoint; should update an already existing product in the database', co.wrap(() => {
+      chai.request(app)
+        .post('/api/v1/products')
+        .set('x-auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im90YWlnYmVAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU0MTQ4NjQ2MH0.F-7ZK_IyOxO5VVKlotO7ySh5QF4Bz2T3qNEg0CxDNSI')
+        .type('form')
+        .send({
+          product_desc: 'bucket',
+          unit_price: 150,
+          quantity_supplied: 10,
+          supplier_name: 'Okonkwo',
+          category: 'hardware',
+        })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res.body).to.be.a.jsonObj();
+          expect(res).to.have.status(200);
+          expect(res.body).to.eql({ message: 'Updated an already existent product.' });
+        });
+    }));
     it('POST / products endpoint; should report a validation error', (done) => {
       chai.request(app)
         .post('/api/v1/products')
@@ -152,79 +152,79 @@ describe('Testing out Products endpoints', () => {
           done();
         });
     });
-    // it('PUT / should Modify the product in the database', co.wrap(() => {
-    //   chai.request(app)
-    //     .put('/api/v1/products/6')
-    //     .set('x-auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im90YWlnYmVAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU0MTQ4NjQ2MH0.F-7ZK_IyOxO5VVKlotO7ySh5QF4Bz2T3qNEg0CxDNSI')
-    //     .type('form')
-    //     .send({
-    //       product_id: 6,
-    //       product_desc: 'Biscuits',
-    //       unit_price: 400,
-    //       quantity_supplied: 150,
-    //       supplier_name: 'Okonkwo',
-    //       category: 'soap',
-    //     })
-    //     .end((err, res) => {
-    //       expect(res).to.have.status(200);
-    //       expect(res.body).to.eql({ message: 'Product Modified' });
-    //     });
-    // }));
-    // it('PUT / should return a forbidden access error', (done) => {
-    //   chai.request(app)
-    //     .put('/api/v1/products/6')
-    //     .set('x-auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuZ2VsYUBnbWFpbC5jb20iLCJhZG1pbiI6ZmFsc2UsImlhdCI6MTU0MTQ4OTE5NH0.aEbvCde9ALV1B0VksKGuu39PIdbWUiGYc5eoigtEAgw')
-    //     .type('form')
-    //     .send({
-    //       product_id: 6,
-    //       product_desc: 'Biscuits',
-    //       unit_price: 400,
-    //       quantity_supplied: 150,
-    //       supplier_name: 'Okonkwo',
-    //       category: 'soap',
-    //     })
-    //     .end((err, res) => {
-    //       expect(res).to.have.status(403);
-    //       expect(res.body).to.eql({ message: 'Forbidden! You need to have  admin privileges' });
-    //       done();
-    //     });
-    // });
-    // it('PUT / should return a validation error', (done) => {
-    //   chai.request(app)
-    //     .put('/api/v1/products/6')
-    //     .set('x-auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im90YWlnYmVAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU0MTQ4NjQ2MH0.F-7ZK_IyOxO5VVKlotO7ySh5QF4Bz2T3qNEg0CxDNSI')
-    //     .type('form')
-    //     .send({
-    //       product_id: 6,
-    //       product_desc: 'Biscuits',
-    //       unit_price: 400,
-    //       quantity_in_stock: 150,
-    //       supplier_name: 'Okonkwo',
-    //       category: 'soap',
-    //     })
-    //     .end((err, res) => {
-    //       expect(res).to.have.status(404);
-    //       done();
-    //     });
-    // });
-    // it('PUT / should return a non existent product error message', co.wrap(() => {
-    //   chai.request(app)
-    //     .put('/api/v1/products/125')
-    //     .set('x-auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im90YWlnYmVAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU0MTQ4NjQ2MH0.F-7ZK_IyOxO5VVKlotO7ySh5QF4Bz2T3qNEg0CxDNSI')
-    //     .type('form')
-    //     .send({
-    //       product_id: 125,
-    //       product_desc: 'sans cream soda',
-    //       unit_price: 400,
-    //       quantity_supplied: 150,
-    //       supplier_name: 'Okonkwo',
-    //       category: 'soap',
-    //     })
-    //     .end((err, res) => {
-    //       expect(res).to.have.status(404);
-    //       expect(res.body).to.eql({ message: 'Product doesn\'t exist! Create the Product' });
-    //     });
-    // }));
+    it('PUT / should Modify the product in the database', co.wrap(() => {
+      chai.request(app)
+        .put('/api/v1/products/6')
+        .set('x-auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im90YWlnYmVAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU0MTQ4NjQ2MH0.F-7ZK_IyOxO5VVKlotO7ySh5QF4Bz2T3qNEg0CxDNSI')
+        .type('form')
+        .send({
+          product_id: 6,
+          product_desc: 'Biscuits',
+          unit_price: 400,
+          quantity_supplied: 150,
+          supplier_name: 'Okonkwo',
+          category: 'soap',
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.eql({ message: 'Product Modified' });
+        });
+    }));
+    it('PUT / should return a forbidden access error', (done) => {
+      chai.request(app)
+        .put('/api/v1/products/6')
+        .set('x-auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuZ2VsYUBnbWFpbC5jb20iLCJhZG1pbiI6ZmFsc2UsImlhdCI6MTU0MTQ4OTE5NH0.aEbvCde9ALV1B0VksKGuu39PIdbWUiGYc5eoigtEAgw')
+        .type('form')
+        .send({
+          product_id: 6,
+          product_desc: 'Biscuits',
+          unit_price: 400,
+          quantity_supplied: 150,
+          supplier_name: 'Okonkwo',
+          category: 'soap',
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(403);
+          expect(res.body).to.eql({ message: 'Forbidden! You need to have  admin privileges' });
+          done();
+        });
+    });
+    it('PUT / should return a validation error', (done) => {
+      chai.request(app)
+        .put('/api/v1/products/6')
+        .set('x-auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im90YWlnYmVAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU0MTQ4NjQ2MH0.F-7ZK_IyOxO5VVKlotO7ySh5QF4Bz2T3qNEg0CxDNSI')
+        .type('form')
+        .send({
+          product_id: 6,
+          product_desc: 'Biscuits',
+          unit_price: 400,
+          quantity_in_stock: 150,
+          supplier_name: 'Okonkwo',
+          category: 'soap',
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          done();
+        });
+    });
+    it('PUT / should return a non existent product error message', co.wrap(() => {
+      chai.request(app)
+        .put('/api/v1/products/125')
+        .set('x-auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im90YWlnYmVAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU0MTQ4NjQ2MH0.F-7ZK_IyOxO5VVKlotO7ySh5QF4Bz2T3qNEg0CxDNSI')
+        .type('form')
+        .send({
+          product_id: 125,
+          product_desc: 'sans cream soda',
+          unit_price: 400,
+          quantity_supplied: 150,
+          supplier_name: 'Okonkwo',
+          category: 'soap',
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          expect(res.body).to.eql({ message: 'Product doesn\'t exist! Create the Product' });
+        });
+    }));
   });
   // end of PUT
   // Beginning of DELETE
